e43153eb3129b8c93ac535c3d45a7462f43ab952
Abdukodiri Kurbonzoda <a.qurbonzoda@gmail.com>
2017-05-03 15:05:38 +0300

    cross-platform reading file

--- Others/Others/Task3All.swift
+++ Others/Others/Task3All.swift
@@ -903,6 +903,47 @@ extension Formula {
 }
 
 
+// Only import only funcs and structs we need to use
+#if os(Linux)
+    // Linux uses Glibc
+    import struct Glibc.FILE
+    import func   Glibc.fopen
+    import func   Glibc.fgets
+    import func   Glibc.fclose
+#else
+    // OS X uses Darwin
+    import struct Darwin.C.FILE
+    import func   Darwin.C.fopen
+    import func   Darwin.C.fgets
+    import func   Darwin.C.fclose
+#endif
+
+///
+/// A simple, cross-platform file parser for reading in files
+///
+struct FileParser {
+    ///
+    /// Reads the contents of a file into a string
+    /// - Parameter path: The path of the file to open
+    /// - Returns: An optional string of the file's contents, or `nil` if the file could
+    ///            not be parsed (i.e., it was not readable)
+    ///
+    static func readFile(path: String) -> String? {
+        let filePointer = fopen(path, "r")
+        guard filePointer != nil else {
+            return nil
+        }
+        // Read in 256b chunks
+        let length = 256
+        var result = String()
+        let buffer = UnsafeMutablePointer<Int8>.allocate(capacity: length)
+        while fgets(buffer, Int32(length), filePointer) != nil {
+            result += String(validatingUTF8: buffer) ?? ""
+        }
+        fclose(filePointer)
+        return result
+    }
+}
 
 
 private let testsDirectory = "/Users/bigdreamer/Programming/logic2015/Others/Task3/Tests/"
@@ -911,7 +952,9 @@ do {
     let inputTests = try FileManager().contentsOfDirectory(atPath: testsDirectory).filter { $0.hasSuffix(".in") }
     
     testCase: for var inputPath in inputTests {
-        let inputFormula = try String(contentsOfFile: testsDirectory + inputPath).toFormula()
+        guard let inputFormula = FileParser.readFile(path: testsDirectory + inputPath)?.toFormula() else {
+            continue testCase
+        }
         let inputFormulaVariables = inputFormula.distinctVariables()
         
         //        print("variables: \(inputFormulaVariables.map { $0.description })")
